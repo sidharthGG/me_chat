fdb8750ea61d06c80e44ec71a9074973d2c84884
sidharth jangid <montypythooon@gmail.com>
2025-07-02 06:47:04 +0000

    Basic Use of ASIO sockets-continued..
    Created basic 'echo' server client to chat in a synchronous manner.
    continous chat.

--- client.cpp
+++ client.cpp
@@ -15,14 +15,22 @@ int main()
     asio::ip::tcp::endpoint endpoint(asio::ip::address::from_string("127.0.0.1"),8080);
     client.connect(endpoint);
 
+    //continue the chat with server in while loop
+    while (true)
+    {
+        asio::streambuf buffer;
+        asio::read_until(client, buffer, '\n');
 
-    asio::streambuf buffer;
-    asio::read_until(client, buffer, '\n');
+        std::istream is(&buffer);
+        std::string message;
+        std::getline(is, message);
+        std::cout<<"server : "<<message<<"\n";
 
-    std::istream is(&buffer);
-    std::string message;
-    std::getline(is, message);
-
-    std::cout<<"message from server :"<<message<<"\n";
+        std::cout<<"client: ";
+        std::string send_message;
+        std::getline(std::cin, send_message);
+        send_message = send_message + '\n'; //for reading on other side
+        asio::write(client, asio::buffer(send_message));
+    }
     return 0;
 }
