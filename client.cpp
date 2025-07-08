b68c532038a8da1474a67fa14060e6f410f27ad8
sidharth jangid <montypythooon@gmail.com>
2025-07-08 16:03:28 +0000

    Single-threaded server client chat.
    Finished the single-threaded server client chat application.
       -added class based code.
       -added prefix-length message framing protocol for message exchange.
       -added asio error handling.

--- client.cpp
+++ client.cpp
@@ -1,36 +1,133 @@
 #include<iostream>
-#include <nlohmann/json.hpp>
 #include <asio.hpp>
 #include <thread>
+#include <functional>
 #include <string>
 #include <memory>
+#define PREFIX_LENGTH 4 // 4 bytes for length prefix
 
-//client side 'echo server' code!!
+class TcpClient{
+    private:
+        int port;
+        asio::io_context& client_context;
+        asio::ip::tcp::socket client_socket;
+    
+    public:
+        TcpClient(asio::io_context& context, int port):
+        port(port),
+        client_context(context),
+        client_socket(client_context)
+        {
+            asio::ip::tcp::endpoint endpoint(asio::ip::address::from_string("127.0.0.1"),8080);
+            client_socket.connect(endpoint); 
+        }
+
+        void run()
+        {
+            while (true)
+            {
+                //write part
+                std::cout<<"client: ";
+                std::string message;
+                std::getline(std::cin, message);
+                if (this->send_message(message))
+                {
+                    std::cout<<"error in send_message, stopped.\n";
+                    break;
+                }
+                
+                //read part
+                message.clear();
+                if (this->read_message(std::ref(message)))
+                {
+                    std::cout<<"error in read_message, stopped.\n";
+                    break;
+                }
+
+                std::cout<<"server: "<<message<<std::endl;                
+            }          
+        }
+
+        std::error_code send_message(std::string message)
+        {
+            std::error_code w_ec;
+            uint32_t message_len = message.length();
+            uint32_t net_len = htonl(message_len);//handle the endian-ness, convert to big-endian
+            std::vector<char> prefix_buffer(PREFIX_LENGTH);
+            memcpy(
+                prefix_buffer.data(),
+                &net_len,
+                PREFIX_LENGTH
+            );
+            asio::write(client_socket, asio::buffer(prefix_buffer), w_ec);
+
+            if (w_ec)
+            {
+                std::cout<<"write error: "<<w_ec.message()<<std::endl;
+                return w_ec;
+            }
+
+            asio::write(client_socket, asio::buffer(message), w_ec);
+
+            if (w_ec)
+            {
+                std::cout<<"write error: "<<w_ec.message()<<std::endl;
+                return w_ec;
+            }
+            
+            return w_ec;
+        }
+
+        std::error_code read_message(std::string& message)
+        {
+        
+            std::error_code r_ec;
+            std::vector<char> buffer(PREFIX_LENGTH);
+            size_t bytes_read = asio::read(client_socket, asio::buffer(buffer, buffer.size()), r_ec);
+
+            if (r_ec)
+            {
+                std::cout<<"read error: "<<r_ec.message()<<std::endl;
+                return r_ec;
+            }
+
+            //std::cout<<"bytes_read: "<<bytes_read<<std::endl;
+
+            uint32_t net_len = 0;
+            memcpy(
+                &net_len,
+                buffer.data(),
+                PREFIX_LENGTH
+            );
+
+            uint32_t buffer_len = ntohl(net_len);
+            message.resize(buffer_len);
+           
+            bytes_read = asio::read(client_socket, asio::buffer(message, message.length()), r_ec);
+
+            if(r_ec)
+            {
+                std::cout<<"read error: "<<r_ec.message()<<std::endl;
+                return r_ec;
+            }
+            
+            return r_ec;
+        }
+
+        
+};
 
 int main()
 {
-    asio::io_context io_context_;
-    asio::ip::tcp::socket client(io_context_);
-
-    asio::ip::tcp::endpoint endpoint(asio::ip::address::from_string("127.0.0.1"),8080);
-    client.connect(endpoint);
-
-    //continue the chat with server in while loop
-    while (true)
+    try
     {
-        asio::streambuf buffer;
-        asio::read_until(client, buffer, '\n');
+        asio::io_context client_context;
+        TcpClient my_client(client_context, 8080);
 
-        std::istream is(&buffer);
-        std::string message;
-        std::getline(is, message);
-        std::cout<<"server : "<<message<<"\n";
-
-        std::cout<<"client: ";
-        std::string send_message;
-        std::getline(std::cin, send_message);
-        send_message = send_message + '\n'; //for reading on other side
-        asio::write(client, asio::buffer(send_message));
+        my_client.run();
     }
-    return 0;
+    catch (std::exception& e)
+    {
+        std::cout<<"exception caught: "<<e.what()<<std::endl;
+    }
 }
--- server.cpp
+++ server.cpp
@@ -1,50 +1,152 @@
 #include<iostream>
-#include <nlohmann/json.hpp>
 #include <asio.hpp>
 #include <thread>
 #include <functional>
 #include <string>
 #include <memory>
+#include <arpa/inet.h>
+#include <thread>
+#define PREFIX_LENGTH 4 // 4 bytes for length prefix
 
-
-void handle_client(asio::ip::tcp::socket client)
+class TcpServer
 {
-    //continue the chat with client in while loop to and fro
-    while (true)
-    {
-        std::cout<<"server : ";
-        std::string message;
-        std::getline(std::cin, message);
-        message = message + '\n'; //getline does not incluce '\n' , we need because client reads until '\n'
-        asio::write(client, asio::buffer(message));
+    private:
+        int server_port;
+        
+        /*have to use lvalue ref in context, since not copyassignable or copyconstructible
+          and we do not use move operators since dont want to move io_context resources. 
+        */
+        asio::io_context& server_context;  
+        asio::ip::tcp::acceptor server_socket;
+    public:
+        TcpServer(asio::io_context& context, int port) : 
+        server_port(port),
+        server_context(context),
+        server_socket(server_context)
+        {
+            server_socket.open(asio::ip::tcp::v4());
+            server_socket.set_option(asio::ip::tcp::acceptor::reuse_address(true));
+            asio::ip::tcp::endpoint server_endpoint(asio::ip::address::from_string("127.0.0.1"),server_port);
+            server_socket.bind(server_endpoint);
+        }
 
-        //start listening message from client side...
-        asio::streambuf buffer;
-        asio::read_until(client, buffer, '\n');
-        std::istream is(&buffer);
-        std::getline(is, message);
-        std::cout<<"client : "<<message<<std::endl;
-    }
-}
+        void run()
+        {
+            this->server_socket.listen();
+
+            while (true) {
+                asio::ip::tcp::socket client_socket(this->server_context);
+                this->server_socket.accept(client_socket);
+                std::cout<<"client connected from "<<client_socket.remote_endpoint()<<std::endl;
+
+                // handle client, send an example message
+                handle_client(std::move(client_socket));
+            }
+        }
+
+        void handle_client(asio::ip::tcp::socket client_socket)
+        {
+            while (true)
+            {
+                std::string message;
+                if (this->read_message(std::ref(client_socket), std::ref(message)) )
+                {
+                    std::cout<<"error in read_message, stopping.\n";
+                    break;
+                }   
+                std::cout<<"client: "<<message<<std::endl;                
+           
+                message.clear();
+
+                std::cout<<"server: ";
+                std::getline(std::cin, message);
+                if (this->send_message(std::ref(client_socket), message))
+                {
+                    std::cout<<"error in send_message, stopping.\n";
+                    break;
+                }
+            }            
+        }
+
+        std::error_code read_message(asio::ip::tcp::socket& client_socket, std::string& message)
+        {
+           
+            std::error_code r_ec;
+            std::vector<char> buffer(PREFIX_LENGTH);
+            size_t bytes_read = asio::read(client_socket, asio::buffer(buffer, buffer.size()), r_ec);
+
+            if (r_ec)
+            {
+                std::cout<<"read error: "<<r_ec.message()<<std::endl;
+                return r_ec;
+            }
+
+            // std::cout<<"bytes_read: "<<bytes_read<<std::endl;
+
+            uint32_t net_len = 0;
+            memcpy(
+                &net_len,
+                buffer.data(),
+                PREFIX_LENGTH
+            );
+
+            uint32_t buffer_len = ntohl(net_len);
+            message.resize(buffer_len);
+          
+            bytes_read = asio::read(client_socket, asio::buffer(message, message.length()), r_ec);
+
+            if(r_ec)
+            {
+                std::cout<<"read error: "<<r_ec.message()<<std::endl;
+                return r_ec;
+            }
+            
+            return r_ec;
+        }
+
+        std::error_code send_message(asio::ip::tcp::socket& client_socket, std::string message)
+        {
+            std::error_code w_ec;
+            uint32_t message_len = message.length();
+            uint32_t net_len = htonl(message_len);//handle the endian-ness, convert to big-endian
+            std::vector<char> prefix_buffer(PREFIX_LENGTH);
+            memcpy(
+                prefix_buffer.data(),
+                &net_len,
+                PREFIX_LENGTH
+            );
+            asio::write(client_socket, asio::buffer(prefix_buffer), w_ec);
+
+            if (w_ec)
+            {
+                std::cout<<"write error: "<<w_ec.message()<<std::endl;
+                return w_ec;
+            }
+
+            asio::write(client_socket, asio::buffer(message), w_ec);
+
+            if (w_ec)
+            {
+                std::cout<<"write error: "<<w_ec.message()<<std::endl;
+                return w_ec;
+            }
+
+            return w_ec;
+        }
+
+};
+
 
 int main()
 {
-    asio::io_context io_context_;
-    asio::ip::tcp::acceptor acceptor(io_context_, asio::ip::tcp::v4());
-    acceptor.set_option(asio::ip::tcp::acceptor::reuse_address(true));
-
-    asio::ip::tcp::endpoint endpoint(asio::ip::address::from_string("127.0.0.1"),8080);
-    acceptor.bind(endpoint);
-
-    acceptor.listen();
-
-    while (true) {
-        asio::ip::tcp::socket client_socket(io_context_);
-        acceptor.accept(client_socket);
-        std::cout<<"client connected from "<<client_socket.remote_endpoint()<<std::endl;
-
-        // handle client, send an example message
-       handle_client(std::move(client_socket));
+    try
+    {
+        asio::io_context server_context;
+        TcpServer server(std::ref(server_context), 8080);
+        server.run();
     }
-    return 0;
+    catch (std::exception& e)
+    {
+        std::cout<<"Exception Caught: "<<e.what()<<std::endl;
+    }
 }
